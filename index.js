a485317a84d975cc1e2c49ded37f3756e43b76bf
Reyderr <wow19973055@gmail.com>
2022-06-23 21:14:52 +0300

    add permission check

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ client.on('messageCreate', msg => {
         case 'test':
             msg.reply(`Test OK. Args: ${args}`);
             break;
-
+        case 'hymn':
+            playHymn(msg);
+            break;
         case 'ping':
             const timeTaken = Date.now() - msg.createdTimestamp;
             msg.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
@@ -29,7 +31,19 @@ client.on('messageCreate', msg => {
 
 
 
-
+function playHymn(message){
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+        return message.channel.send(
+            "You need to be in a voice channel to play music!"
+        );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        return message.channel.send(
+            "I need the permissions to join and speak in your voice channel!"
+        );
+    }
+}
 
 
 
